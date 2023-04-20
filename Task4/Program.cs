041f25d96c50d5ca09fbc8a3c600178db2be01cb
ArhyParh <zescoarty@gmail.com>
2023-04-20 22:35:48 +0300

    Решил задачу номер 7

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+	// 456 -> 6
+	// 782 -> 2
+	// 918 -> 8
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+while(number>=100)
+{
+    number -=100;
+}
+while(number>=10) 
+{
+    number -= 10;
+}
+Console.WriteLine(number);
